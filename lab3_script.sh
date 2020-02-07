a47ecb2a57276004f25ea76d14bddd01347bd381
Ciro Santaniello <cirojsantaniello@gmail.com>
2020-02-07 11:10:05 -0700

    Adding all of the files for lab 3

--- lab3_script.sh
+++ lab3_script.sh
@@ -5,28 +5,36 @@
 #Problem 1 Code:
 #Make sure to document how you are solving each problem
 
+#inputs for user
 echo "Input file name"
 read filename
 echo "Input RegEx Pattern"
 read pattern
 
+#outputs the users inputs for their reference
 echo "Filename: $filename"
 echo "Pattern: $pattern"
 
+#preforms the search
 grep $pattern $filename
 
+#searches and outputs the number of phone numbers in regex_practice.txt
 grep -c '[0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}'  "regex_practice.txt"
 
+#searches and counts all emails and stores value in a variable
 let emails=$(grep -c "@" "regex_practice.txt")
 echo "number of emails: $emails"
 
+#prints all phone numbers with 303 area code
 echo "Printing numbers with 303 area code"
 grep "303" "regex_practice.txt"
+
 
 #"storing all geocities emails, saving to file, then printing file
 echo "saving all geocities emails to email_results.txt"
 grep "geocities.com" "regex_practice.txt" >> email_results.txt
 
+uploads email_results.txt to git
 git add email_results.txt
 git commit email_results.txt
 git push origin master
